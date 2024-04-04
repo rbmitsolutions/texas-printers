ff089f5c86e399e7b73becf5e46b9c1241f69d64
Raphael Brandão Mesquita <raphaelbmesquita@gmail.com>
2024-04-04 12:49:07 +0100

    fix: order template

--- src/templates/orderControllerTemplate.ts
+++ src/templates/orderControllerTemplate.ts
@@ -10,20 +10,19 @@ import { getTimeFromDate } from "../utils/convert";
 const orderControllerTemplate = (orders: IOrder[], table: ITable, orderControllerNumber: number) => {
   const fontBoldTitle = "\x1B\x21\x31";
   const fontBoldBiggerTitle = "\x1B\x21\x34"
-  const fontSmall = "\x1B\x21\x26";
+  const fontSmall = "\x1B\x21\x24";
+
+  const lineSpacing = "\x1B\x33\x60"
 
   const print = [
     `
-${fontBoldTitle}
-
+${lineSpacing}
 
 
 
 ${fontBoldBiggerTitle}
 T - ${table?.number}   [${table?.guests}]   ${getTimeFromDate(new Date())}
-
 ${table?.meal_status === 'all together' ? 'All Together' : ''}
-${fontBoldTitle}
 `,
   ];
   for (const menuType of menuTypes) {
@@ -32,25 +31,23 @@ ${fontBoldTitle}
     );
 
     if (ordersByType && ordersByType.length) {
-      print.push(`\n`);
       print.push(`${fontBoldTitle}`);
+      print.push(`\n`);
       print.push(`${centerTextIn28Chars(menuTypeLabels[menuType.type])}`);
       print.push(`\n`);
       for (const order of ordersByType) {
-        print.push(`\n`);
-        print.push(`${fontBoldBiggerTitle}`);
         print.push(`${order.quantity}  ${order.menu_short_title}`);
+        print.push(`\n`);
         print.push(`${fontBoldTitle}`);
         if (order?.add_ons?.length > 0) {
           // Set a small font size for the description
-          print.push(`\n`);
           print.push(`${fontSmall}`);
           order?.add_ons?.forEach((x) => {
             print.push(` >>${x?.title}`)
-            print.push(`\n`);
           });
-          // Reset to the original font size
+          print.push(`\n`);
           print.push(`${fontBoldTitle}`);
+          // Reset to the original font size
         }
       }
     }
